bf8dbec79eba3a106dd391fc387d602bf7e96290
aldojoss <168256201+aldojoss@users.noreply.github.com>
2024-07-03 21:58:42 -0600

    Add files via upload

---
+++ ELECTROCOST/src/Modelo.java
@@ -0,0 +1,50 @@
+public abstract class Modelo {
+
+    //Protegido
+    private String name;
+    private int usodiario;
+    private double volteos;
+    private double consumomensual;
+
+//Esta protegido para que solo sus calses hijas puedan usarlo
+public Modelo(String name, int usodiario, double volteos, double consumomensual){
+        this.name = name;
+        this.usodiario = usodiario;
+        this.volteos = volteos;
+        this.consumomensual = consumomensual;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public int getUsodiario() {
+    return usodiario;
+}
+
+public void setUsodiario(int usodiario) {
+    this.usodiario = usodiario;
+}
+
+public double getConsumomensual() {
+    return consumomensual;
+}
+public void setConsumomensual(double consumomensual) {
+    this.consumomensual = consumomensual;
+}
+
+public double getVolteos() {
+    return volteos;
+}
+public void setVolteos(double volteos) {
+    this.volteos = volteos;
+}
+
+//METODO ABSTRACTO
+public abstract String toString();
+
+}
